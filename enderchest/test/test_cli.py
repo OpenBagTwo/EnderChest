aca262476295ab66cd2a6490921647fb84901b0d
OpenBagTwo <openbagtwo@gmail.com>
2022-10-05 23:56:28 -0400

    Finish craft tests

--- enderchest/test/test_cli.py
+++ enderchest/test/test_cli.py
@@ -6,6 +6,7 @@ import pytest
 
 import enderchest
 from enderchest import cli
+from enderchest.sync import Remote
 
 
 class TestHelp:
@@ -42,20 +43,101 @@ class TestCraft:
         _, root, _ = cli.parse_args(["enderchest", "craft"])
         assert root == Path("~~dummy~~")
 
-    def test_default_dispatch_is_to_non_config_craft(self, monkeypatch):
+    def test_default_dispatch_is_to_non_config_craft(self, monkeypatch, capsys):
         def wrong_method(*args, **kwargs):
             assert False, "Wrong method"
 
         def correct_method(*args, **kwargs):
-            pass
+            print("You chose correctly")
 
         monkeypatch.setattr(cli, "craft_ender_chest_from_config", wrong_method)
         monkeypatch.setattr(cli, "craft_ender_chest", correct_method)
-        action, _, _ = cli.parse_args(["enderchest", "craft"])
+        action, root, _ = cli.parse_args(["enderchest", "craft"])
+        action(root)
+        assert capsys.readouterr().out == "You chose correctly\n"
 
-    def test_first_argument_is_root(self):
+    def test_first_argument_is_root(self, capsys):
         _, root, _ = cli.parse_args(["enderchest", "craft", "/home"])
         assert root == Path("/home")
+
+    @pytest.mark.parametrize("config_flag", ("-f", "--file"))
+    def test_passing_a_config_switches_to_the_config_craft(
+        self, monkeypatch, capsys, config_flag
+    ):
+        def wrong_method(*args, **kwargs):
+            assert False, "Wrong method"
+
+        def correct_method(path):
+            print(path)
+
+        monkeypatch.setattr(cli, "craft_ender_chest_from_config", correct_method)
+        monkeypatch.setattr(cli, "craft_ender_chest", wrong_method)
+        action, root, options = cli.parse_args(
+            ["enderchest", "craft", config_flag, "blah.cfg"]
+        )
+
+        action(root, **options)
+        assert capsys.readouterr().out == "blah.cfg\n"
+
+    @pytest.mark.parametrize("remote_flag", ("-r", "--remote"))
+    def test_passing_in_a_single_remote(self, monkeypatch, remote_flag):
+
+        remotes = []
+
+        def wrong_method(*args, **kwargs):
+            assert False, "Wrong method"
+
+        def correct_method(path, *parsed_remotes, **kwargs):
+            remotes.extend(parsed_remotes)
+
+        monkeypatch.setattr(cli, "craft_ender_chest_from_config", wrong_method)
+        monkeypatch.setattr(cli, "craft_ender_chest", correct_method)
+
+        action, root, options = cli.parse_args(
+            ["enderchest", "craft", remote_flag, "openbagtwo@mirror:~/minecraft"]
+        )
+        action(root, **options)
+
+        assert remotes == [Remote("mirror", "~/minecraft", "openbagtwo")]
+
+    @pytest.mark.parametrize("remote_flag_1", ("-r", "--remote"))
+    @pytest.mark.parametrize("remote_flag_2", ("-r", "--remote"))
+    @pytest.mark.parametrize("remote_flag_3", ("-r", "--remote"))
+    def test_passing_in_a_multiple_remotes_plus_other_kwargs(
+        self, monkeypatch, remote_flag_1, remote_flag_2, remote_flag_3
+    ):
+        remotes = []
+
+        def wrong_method(*args, **kwargs):
+            assert False, "Wrong method"
+
+        def correct_method(path, *parsed_remotes, **kwargs):
+            assert kwargs["overwrite"]
+            remotes.extend(parsed_remotes)
+
+        monkeypatch.setattr(cli, "craft_ender_chest_from_config", wrong_method)
+        monkeypatch.setattr(cli, "craft_ender_chest", correct_method)
+
+        action, root, options = cli.parse_args(
+            [
+                "enderchest",
+                "craft",
+                remote_flag_1,
+                "openbagtwo@mirror:~/minecraft",
+                remote_flag_2,
+                "~/minecraft2",
+                "--overwrite",
+                remote_flag_3,
+                "pie:/opt/run/minecraft",
+            ]
+        )
+        action(root, **options)
+
+        assert remotes == [
+            Remote("mirror", "~/minecraft", "openbagtwo"),
+            Remote(None, "~/minecraft2"),
+            Remote("pie", "/opt/run/minecraft"),
+        ]
 
 
 class TestPlace:
