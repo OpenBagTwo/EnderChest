be44788e3b6d8a5f83f6428bbb846e9469c68cb5
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2023-05-27 15:57:14 -0400

    remove debug line

--- enderchest/craft.py
+++ enderchest/craft.py
@@ -534,7 +534,7 @@ def specify_shulker_box_from_prompt(minecraft_root: Path, name: str) -> ShulkerB
 
         # TODO: stop wastefully reloading the cfg
         host = load_ender_chest(minecraft_root).name
-        print(hosts, host)
+
         if not any(
             fnmatch.fnmatchcase(host.lower(), host_spec.lower()) for host_spec in hosts
         ):
