512d36a1ed15419552bc7a5c28259afdfc308b99
docs-golem <losthemobvote@openbagtwo.net>
2023-06-15 01:59:40 +0000

    Deployed 0c14997 to v0.1.2 with MkDocs 1.4.3 and mike 1.1.2

---
+++ v0.1.2/gen_clidocs.py
@@ -0,0 +1,25 @@
+"""Generate CLI docs"""
+import mkdocs_gen_files
+
+from enderchest import cli
+
+root_parser, verb_parsers = cli.generate_parsers()
+
+cli_guide = f"""
+# Full Command-Line Interface Documentation
+
+## Summary
+```bash
+{root_parser.format_help()}```
+"""
+
+for verb, parser in verb_parsers.items():
+    level = len(f"enderchest {verb}".split())
+    cli_guide += f"""
+{"#"*level} `enderchest {verb}`
+```bash
+{parser.format_help()}```
+"""
+
+with mkdocs_gen_files.open("cli.md", "w") as cli_docs:
+    cli_docs.write(cli_guide)
