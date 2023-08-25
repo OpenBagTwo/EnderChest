68f351ad9623c272ea5655d2b87efc0c82db2add
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2023-08-25 14:39:28 -0400

    Manually generate sync reports

--- enderchest/test/test_sync_utils.py
+++ enderchest/test/test_sync_utils.py
@@ -1,4 +1,5 @@
 """Tests of the sync-helper utilities"""
+import logging
 import os
 import shutil
 import time
@@ -94,57 +95,82 @@ class TestIsIdentical:
         assert not sync_utils.is_identical(one.stat(), two.stat())
 
 
+@pytest.fixture
+def file_system(tmp_path):
+    source_tree = tmp_path / "oak"
+    (source_tree / "branch").mkdir(parents=True)
+    (source_tree / "branch" / "twig").mkdir()
+    (source_tree / "branch" / "leaf").write_text("green")
+    (source_tree / "branch" / "acorn").touch()
+    (source_tree / "branch2").mkdir()
+    (source_tree / "branch2" / "acorn").touch()
+
+    dst_tree = tmp_path / "birch"
+    (dst_tree / "branch").mkdir(parents=True)
+    (dst_tree / "branch" / "twig").mkdir()
+    (dst_tree / "branch" / "leaf").write_text("yellow")
+    (dst_tree / "branch" / "acorn").touch()
+    (dst_tree / "beehive").touch()
+    (dst_tree / "root").mkdir()
+    (dst_tree / "root" / "cicada").touch()
+
+    yield tmp_path
+
+
+@pytest.fixture
+def diff(file_system):
+    yield sync_utils.diff(
+        file.get_contents(file_system / "oak"),
+        file.get_contents(file_system / "birch"),
+    )
+
+
 class TestDiff:
-    @pytest.fixture
-    def file_system(self, tmp_path):
-        source_tree = tmp_path / "oak"
-        (source_tree / "branch").mkdir(parents=True)
-        (source_tree / "branch" / "twig").mkdir()
-        (source_tree / "branch" / "leaf").write_text("green")
-        (source_tree / "branch" / "acorn").touch()
-        (source_tree / "branch2").mkdir()
-        (source_tree / "branch2" / "acorn").touch()
-
-        dst_tree = tmp_path / "birch"
-        (dst_tree / "branch").mkdir(parents=True)
-        (dst_tree / "branch" / "twig").mkdir()
-        (dst_tree / "branch" / "leaf").write_text("yellow")
-        (dst_tree / "branch" / "acorn").touch()
-        (dst_tree / "beehive").touch()
-        (dst_tree / "root").mkdir()
-        (dst_tree / "root" / "cicada").touch()
-
-        yield tmp_path
-
-    @pytest.fixture
-    def diff(self, file_system):
-        yield sync_utils.diff(
-            file.get_contents(file_system / "oak"),
-            file.get_contents(file_system / "birch"),
-        )
-
     def test_diff_omits_identical_contents(self, diff):
         assert {
             Path("branch") / "acorn",
             Path("branch") / "twig",
             Path("branch"),
-        }.intersection((f for f, _ in diff)) == set()
+        }.intersection((f for f, *_ in diff)) == set()
 
     def test_diff_captures_updates(self, diff):
-        assert [Path("branch") / "leaf"] == [f for f, op in diff if op == Op.REPLACE]
+        assert [Path("branch") / "leaf"] == [f for f, _, op in diff if op == Op.REPLACE]
 
     def test_diff_captures_creation_of_files_and_folders(self, diff):
         assert [
             Path("branch2"),
             Path("branch2") / "acorn",
-        ] == [f for f, op in diff if op == Op.CREATE]
+        ] == [f for f, _, op in diff if op == Op.CREATE]
 
     def test_diff_captures_deletion_of_files_and_folders(self, diff):
         assert [
             Path("root") / "cicada",
             Path("beehive"),
             Path("root"),
-        ] == [f for f, op in diff if op == Op.DELETE]
+        ] == [f for f, _, op in diff if op == Op.DELETE]
+
+
+class TestGenerateSyncReport:
+    def test_generate_sync_report(self, diff, caplog):
+        expected = [
+            "Deleting beehive",
+            """Within branch...
+  - Creating 0 files
+  - Replacing 1 file
+  - Deleting 0 files""",
+            "Creating branch2",
+            "Deleting root",
+        ]
+
+        sync_utils.generate_sync_report(diff)
+
+        info_logs = [
+            (record.msg % record.args).strip()
+            for record in caplog.records
+            if record.levelno == logging.INFO
+        ]
+
+        assert info_logs == expected
 
 
 class TestFileIgnorePatternBuilder:
