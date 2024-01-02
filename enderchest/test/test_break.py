05fdeb5332903a777cb534b9619be615192282d4
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2024-01-02 15:35:51 -0500

    A **much more direct** way to implement break
    Because we don't care if EnderChest placed the link--we just care about whether the link goes THROUGH the EnderChest

--- enderchest/test/test_break.py
+++ enderchest/test/test_break.py
@@ -39,7 +39,7 @@ class TestBreakEnderChest:
     def test_break_asks_you_if_youre_really_sure(
         self, minecraft_root, home, monkeypatch, capsys, caplog
     ):
-        script_reader = utils.scripted_prompt([""] * 2)
+        script_reader = utils.scripted_prompt([""])
         monkeypatch.setattr("builtins.input", script_reader)
 
         utils.pre_populate_enderchest(
@@ -59,43 +59,15 @@ class TestBreakEnderChest:
 
         assert len(errorish_log) == 2
 
-    @pytest.mark.usefixtures("multi_box_setup_teardown")
-    def test_break_allows_you_to_proceed_even_when_there_are_conflicts(
-        self, minecraft_root, home, monkeypatch, capsys, caplog
-    ):
-        script_reader = utils.scripted_prompt([""] * 2)
-        monkeypatch.setattr("builtins.input", script_reader)
 
-        uninstall.break_ender_chest(minecraft_root)
-
-        _ = capsys.readouterr()  # suppress outputs
-        errorish_log = [
-            record for record in caplog.records if record.levelno >= logging.WARNING
-        ]
-
-        assert (
-            errorish_log[0].levelno == logging.ERROR
-            and errorish_log[0].message.startswith("Error linking")
-            and "options.txt" in errorish_log[0].message
-        )
-
-        assert errorish_log[-2].message.startswith("Are you sure")
-
-        assert len(errorish_log) == 3
-
-
-@pytest.mark.usefixtures("multi_box_setup_teardown")
 class TestBreaking:
-    @pytest.fixture
-    def placement_cache(self, minecraft_root, capsys, caplog):
-        """Setup/teardwon for this class"""
+    @pytest.fixture(autouse=True)
+    def setup_teardown(self, minecraft_root, multi_box_setup_teardown, capsys, caplog):
         utils.pre_populate_enderchest(
             fs.ender_chest_folder(minecraft_root, check_exists=False),
             *utils.TESTING_SHULKER_CONFIGS
         )
-        placement_cache = place.place_ender_chest(
-            minecraft_root, relative=False, error_handling="ignore"
-        )
+        place.place_ender_chest(minecraft_root, relative=False, error_handling="ignore")
 
         errorish_log = [
             record for record in caplog.records if record.levelno >= logging.WARNING
@@ -107,26 +79,12 @@ class TestBreaking:
             and "options.txt" in errorish_log[0].message
         )
 
-        # assert that the placement cache is clean
-        assert not placement_cache["official"].get(Path("options.txt"), [])
-
-        yield placement_cache
+        yield
 
         _ = capsys.readouterr()  # suppress outputs
 
-    @pytest.fixture
-    def instance_lookup(self, minecraft_root):
-        yield {
-            instance.name: instance
-            for instance in gather.load_ender_chest_instances(
-                minecraft_root, logging.DEBUG
-            )
-        }
-
-    def test_clean_uninstall_emits_no_warnings(
-        self, minecraft_root, placement_cache, instance_lookup, caplog
-    ):
-        uninstall._break(minecraft_root, instance_lookup, placement_cache)
+    def test_clean_uninstall_emits_no_warnings(self, minecraft_root, caplog):
+        uninstall._break(fs.ender_chest_folder(minecraft_root), utils.TESTING_INSTANCES)
         warnings = [
             record for record in caplog.records if record.levelno >= logging.WARNING
         ]
@@ -135,56 +93,37 @@ class TestBreaking:
     @pytest.mark.parametrize(
         "instance", (instance.name for instance in utils.TESTING_INSTANCES)
     )
-    def test_uninstall_replaces_links_with_copies(
-        self, minecraft_root, placement_cache, instance_lookup, instance
-    ):
-        resource_path = instance_lookup[instance].root.expanduser() / "usercache.json"
+    def test_uninstall_replaces_links_with_copies(self, minecraft_root, instance):
+        resource_path = {
+            instance.name: instance for instance in utils.TESTING_INSTANCES
+        }[instance].root.expanduser() / "usercache.json"
 
         # meta-test
         assert resource_path.readlink().is_relative_to(
             fs.ender_chest_folder(minecraft_root)
         )
-        uninstall._break(minecraft_root, instance_lookup, placement_cache)
+        uninstall._break(fs.ender_chest_folder(minecraft_root), utils.TESTING_INSTANCES)
 
         assert resource_path.exists()
         assert not resource_path.is_symlink()
         assert resource_path.read_text() == "alexander\nmomoa\nbateman\n"
 
-    @pytest.mark.parametrize("instance", ("axolotl", "bee"))
-    def test_uninstall_copies_based_on_highest_priority(
-        self,
-        minecraft_root,
-        placement_cache,
-        instance_lookup,
-        instance,
-    ):
-        resource_path = (
-            instance_lookup[instance].root.expanduser() / "resourcepacks" / "stuff.zip"
-        )
-        uninstall._break(minecraft_root, instance_lookup, placement_cache)
-
-        assert resource_path.exists()
-        assert not resource_path.is_symlink()
-        assert (
-            resource_path.read_text() == "optifine-optimized!"
-            if instance == "bee"
-            else "dfgwhgsadfhsd"
-        )
-
     @pytest.mark.parametrize(
         "instance", (instance.name for instance in utils.TESTING_INSTANCES)
     )
     def test_uninstall_replaces_links_pointing_outside_the_chest_with_links(
-        self, minecraft_root, placement_cache, instance_lookup, instance
+        self, minecraft_root, instance
     ):
-        resource_path = instance_lookup[instance].root.expanduser() / "crash-reports"
+        resource_path = {
+            instance.name: instance for instance in utils.TESTING_INSTANCES
+        }[instance].root.expanduser() / "crash-reports"
 
         # meta-test
         assert not resource_path.readlink().is_relative_to(
             fs.ender_chest_folder(minecraft_root)
         )
 
-        uninstall._break(minecraft_root, instance_lookup, placement_cache)
+        uninstall._break(fs.ender_chest_folder(minecraft_root), utils.TESTING_INSTANCES)
 
         assert resource_path.exists()
         assert resource_path.is_symlink()
@@ -194,57 +133,47 @@ class TestBreaking:
             .endswith("And somehow also on fire\n")
         )
 
-    def test_uninstall_proceeds_past_conflicts(
-        self, minecraft_root, instance_lookup, placement_cache, caplog
-    ) -> None:
-        # though these should be caught during the generation of the placement cache
-
-        # Normally the order of placements cannot be guaranteed, so we can't test
-        # match-skips vs. ignores
-        # (see: `test_place.py::TestMultiShulkerPlacing::test_ignore_failures
-        # but also... # TODO sort `place._rglob`?)
-        # but because dicts in Python 3.6/7+ are ordered dicts, we can manipulate
-        # the placement cache to GUARANTEE that the conflict happens first
-        home_placements: dict[Path, list[str]] = {Path("options.txt"): ["1.19"]}
-        home_placements.update(placement_cache["official"])
-        placement_cache["official"] = home_placements
-        # meta-test
-        assert list(placement_cache["official"].keys())[0] == Path("options.txt")
-
+    @pytest.mark.parametrize("instance", ("axolotl", "bee"))
+    def test_uninstall_copies_based_on_highest_priority(
+        self,
+        minecraft_root,
+        instance,
+    ):
         resource_path = (
-            instance_lookup["official"].root.expanduser() / "config" / "iris.properties"
+            {instance.name: instance for instance in utils.TESTING_INSTANCES}[
+                instance
+            ].root.expanduser()
+            / "resourcepacks"
+            / "stuff.zip"
         )
+        uninstall._break(fs.ender_chest_folder(minecraft_root), utils.TESTING_INSTANCES)
 
-        uninstall._break(minecraft_root, instance_lookup, placement_cache)
-
-        errorish_log = [
-            record for record in caplog.records if record.levelno >= logging.WARNING
-        ]
-        assert len(errorish_log) == 1
-
-        assert "options.txt already exists" in errorish_log[0].message
         assert resource_path.exists()
-        assert not resource_path.is_symlink()
-        assert resource_path.read_text() == "flower or aperture?\n"
+        assert resource_path.is_symlink() is (instance != "bee")
+        assert (
+            resource_path.read_text() == "optifine-optimized!"
+            if instance == "bee"
+            else "dfgwhgsadfhsd"
+        )
 
     @pytest.mark.parametrize(
         "instance", (instance.name for instance in utils.TESTING_INSTANCES)
     )
     def test_uninstall_proceeds_past_broken_links(
-        self, minecraft_root, instance_lookup, placement_cache, instance, caplog
+        self, minecraft_root, instance, caplog
     ):
         (fs.shulker_box_root(minecraft_root, "global") / "backups").rmdir()
 
-        # should be safe, because the link folders *are* linked deterministically,
-        # in the order set by the shulker box config
-        resource_path = instance_lookup["Chest Boat"].root.expanduser() / "logs"
+        resource_path = {
+            instance.name: instance for instance in utils.TESTING_INSTANCES
+        }["Chest Boat"].root.expanduser() / "logs"
 
-        uninstall._break(minecraft_root, instance_lookup, placement_cache)
+        uninstall._break(fs.ender_chest_folder(minecraft_root), utils.TESTING_INSTANCES)
 
         errorish_log = [
             record for record in caplog.records if record.levelno >= logging.WARNING
         ]
-        assert len(errorish_log) == len(instance_lookup)
+        assert len(errorish_log) == len(utils.TESTING_INSTANCES)
 
         assert all(
             ("No such file or directory" in record.message for record in errorish_log)
@@ -255,17 +184,22 @@ class TestBreaking:
             (resource_path / "bumpona.log").read_text("utf-8").startswith("Like a bump")
         )
 
-    def test_break_doesnt_touch_links_created_outside_of_enderchest(
-        self, minecraft_root, placement_cache, instance_lookup, caplog
+    def test_break_doesnt_touch_links_pointing_directly_outside_of_enderchest(
+        self, minecraft_root, caplog
     ):
         # even broken ones
         resource_path = (
-            instance_lookup["bee"].root.expanduser() / "config" / "some_mod.json"
+            {instance.name: instance for instance in utils.TESTING_INSTANCES}[
+                "bee"
+            ].root.expanduser()
+            / "config"
+            / "some_mod.json"
         )
+
         os.symlink(
             minecraft_root / "null" / "null", resource_path, target_is_directory=False
         )
-        uninstall._break(minecraft_root, instance_lookup, placement_cache)
+        uninstall._break(fs.ender_chest_folder(minecraft_root), utils.TESTING_INSTANCES)
         warnings = [
             record for record in caplog.records if record.levelno >= logging.WARNING
         ]
